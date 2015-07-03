120a49e13a7e0467b74b2653fd014e7b02aecb47
Jensen Bouzi <jbouzi12@gmail.com>
2015-07-03 18:35:16 -0400

    create routes/todos file

---
+++ routes/todos.js
@@ -0,0 +1,16 @@
+// var express = require('express');
+// var router = express.Router();
+
+// router.get('/todos', function(req, res) {
+//   db.todos.find({}, function(err, tasks) {
+//     if (err) {
+//       console.log('error occured: ', error);
+//       return;
+//     }
+//     var response = {
+//       tasks: tasks
+//     };
+//     res.json(response);
+//   });
+// });
+// module.exports = router;
