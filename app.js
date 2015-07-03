14563dbfdcbbc821e6e0b1021f900f5512d612e2
Jensen Bouzi <jbouzi12@gmail.com>
2015-07-03 18:34:50 -0400

    configure database and create /todos route

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var path = require('path');
+var http = require('http');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var dbUrl = "todo";
+var collections = ["todos"];
+var mongojs = require('mongojs');
+var db = mongojs(dbUrl, collections);
+
 var routes = require('./routes/index');
+var todos = require('./routes/todos');
 
 var app = express();
 
@@ -21,7 +28,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', routes);
+app.get('/todos', function(req, res) {
+  db.todos.find({}, function(err, tasks) {
+    if (err) {
+      console.log('error occured: ', error);
+      return;
+    }
+    var response = {
+      tasks: tasks
+    };
+    res.json(response);
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
