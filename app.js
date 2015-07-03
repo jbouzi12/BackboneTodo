a0ff12e29c03ddaf9b05a6c63cdf02fb4e929198
Jensen Bouzi <jbouzi12@gmail.com>
2015-07-03 19:10:17 -0400

    define route for individual task

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', routes.todos.all);
+app.get('/:title', routes.todos.one);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
