fa87602c152ce8a9586d4a11993691f40df84f45
Jensen Bouzi <jbouzi12@gmail.com>
2015-07-03 19:19:22 -0400

    change route from /:title to /:id

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', routes.todos.all);
-app.get('/:title', routes.todos.one);
+app.get('/:id', routes.todos.one);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
