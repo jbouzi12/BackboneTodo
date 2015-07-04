e0094a37f5e105979a7252aa145f73c9206244ed
Jensen Bouzi <jbouzi12@gmail.com>
2015-07-04 12:05:48 -0400

    begin creating task view

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -11,3 +11,17 @@ var taskCollection = Backbone.Model.extend({
 	model: Task,
 	url: "/"
 });
+
+var TaskView = Backbone.View.extend({
+	tagName: "li",
+
+	className: "task",
+	
+	template: _.template($(#tasktemplate).html()),
+	
+	render: function(){
+		this.$el.html(this.template(this.model.toJSON()));
+		return this
+
+	}
+});
