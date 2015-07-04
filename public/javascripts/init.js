01e772923001a3d34cbc1e3c66cd1a920c9c4e15
Jensen Bouzi <jbouzi12@gmail.com>
2015-07-04 13:45:39 -0400

    refactor init method to use AppRouter

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,11 +1,5 @@
 $(function(){
 	// Initialize client with backbone
-	var collection = new TaskCollection();
-	collection.fetch({
-		success:function(data) {
-			var view = new TaskCollectionView({collection: data});
-			// will change where tasks will be appended to
-			$('body').append(view.render().el);
-		}
-	})
+	new AppRouter();
+	Backbone.history.start();
 });
