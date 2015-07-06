505139fc104fb3fb24fe402c1b548f32975797a6
Jensen Bouzi <jbouzi12@gmail.com>
2015-07-05 22:43:21 -0400

    add function for completing a task in task view

--- public/javascripts/views/task-view.js
+++ public/javascripts/views/task-view.js
@@ -9,17 +9,31 @@ var myApp = myApp || {};
 		className: "task",
 
 		events: {
-			'click .destroy': 'deleteTask'
+			'click .destroy': 'deleteTask',
+			'click .complete': 'completeTask'
 		},
 
 		initialize: function() {
 			this.listenTo(this.model, 'destroy', this.remove);
+			this.listenTo(this.model, 'change', this.render)
 		},
 
 		deleteTask: function() {
 			this.model.destroy();
 		},
 		
+		completeTask: function() {
+
+			if (this.$el.hasClass('completed')) {
+				alert('You have already completed this task. Try getting the others done');
+				return;
+			} else {
+				this.model.save({ completed: true });
+				this.model.trigger('change');
+				this.$el.addClass('completed');
+			}
+		},
+
 		render: function(){
 	      var template = $('#tasktemplate').html();
 	      var compiled = Handlebars.compile(template);
